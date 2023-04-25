4201b9c2f62d803f60c5a80ea3280f57fae7d709
davidovits1 <david.o3111@gmail.com>
2023-04-25 23:43:32 +0300

    finish monkey class

--- include/Monkey.h
+++ include/Monkey.h
@@ -1,1 +1,16 @@
 #pragma once
+#include "Animal.h"
+class Monkey : public Animal {
+	int count;
+	int numMove;
+public:
+
+	Monkey();
+	Monkey(const std::string& n, const Location& l) : Animal(n, l);
+
+	void printDetails() const override;
+	char getInitial() const override;
+	void step() override;
+	void turnVertically() override;
+	void turnHorizontally() override;
+};
